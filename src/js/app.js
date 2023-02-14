6a5c918bc829732fe3e8dd65756fafd055d45edc
Andrew Bardukov <arhandreu@gmail.com>
2023-02-14 10:58:16 +0500

    14-02-2023_10-58

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,24 @@
 if (typeof window !== 'undefined') {
   (() => {
-    let playing = true,
-      activeHole = 1;
+    let playing = true;
+    let activeHole = 1;
 
-    const stop = () => playing = true,
-      getHole = index => document.getElementById(`hole${index}`),
-      deactivateHole = index =>
-        getHole( index ).className = 'hole',
-      activateHole = index =>
-        getHole( index ).className = 'hole hole_has-mole',
-      next = () => setTimeout(() => {
-        if ( !playing ) {
-          return;
-        }
-        deactivateHole( activeHole );
-        activeHole = Math.floor( 1 + Math.random() * 16 );
-        activateHole( activeHole );
-        next();
-      }, 800 );
+    const stop = () => playing = true;
+    const getHole = (index) => document.getElementById(`hole${index}`);
+    const deactivateHole = (index) => getHole(index).className = 'hole';
+    const activateHole = (index) => getHole(index).className = 'hole hole_has-mole';
+    const next = () => setTimeout(() => {
+      if (!playing) {
+        return;
+      }
+      deactivateHole(activeHole);
+      activeHole = Math.floor(1 + Math.random() * 16);
+      activateHole(activeHole);
+      next();
+    }, 800);
 
     next();
   })();
 } else {
-  console.log('You are on the server')
+  // console.log('You are on the server');
 }
