a1dfdd273c21007cc3d405af6275bcfc657b9af4
Andrew Bardukov <arhandreu@gmail.com>
2023-02-21 19:09:33 +0500

    2023-02-21_19-09

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Webpack5
 
-[![Build status](https://ci.appveyor.com/api/projects/status/q2nvo3bsgmt4006p/branch/main?svg=true)](https://ci.appveyor.com/project/arhandreu/js-mole/branch/main)
+[![Build status](https://ci.appveyor.com/api/projects/status/g1qnxi47l58vda62?svg=true)](https://ci.appveyor.com/project/arhandreu/js-mole-event)
 
 [Информация о релизе Webpack 5](https://webpack.js.org/blog/2020-10-10-webpack-5-release/)
 
--- src/js/app.js
+++ src/js/app.js
@@ -20,31 +20,29 @@ if (typeof window !== 'undefined') {
     next();
   })();
 
-getHole = index => document.getElementById(`hole${index}`) 
-for (index=1; index <= 9; index++) {        
+  const getHole = (index) => document.getElementById(`hole${index}`);
+  for (let index = 1; index <= 9; index += 1) {
     getHole(index).onclick = (click) => {
-        if (click.target.className.includes('hole hole_has-mole')) {
-            dead = document.getElementById(`dead`);            
-            dead.textContent = Number(dead.textContent) + 1
-            click.target.className = 'hole'
-            if (Number(dead.textContent) === 10) {
-                dead.textContent = 0
-                lost.textContent = 0
-                alert('Вы выиграли')
-            }
-        }    
-        else {
-            lost = document.getElementById(`lost`);            
-            lost.textContent = Number(lost.textContent) + 1
-            if (Number(lost.textContent) === 10) {
-                dead.textContent = 0
-                lost.textContent = 0
-                alert('Вы проиграли')                
-            }
-        }    
-        
-    }    
-}
+      if (click.target.className.includes('hole hole_has-mole')) {
+        const dead = document.getElementById('dead');
+        dead.textContent = Number(dead.textContent) + 1;
+        click.target.className = 'hole';
+        if (Number(dead.textContent) === 5) {
+          dead.textContent = 0;
+          lost.textContent = 0;
+          alert('Вы выиграли');
+        }
+      } else {
+        const lost = document.getElementById('lost');
+        lost.textContent = Number(lost.textContent) + 1;
+        if (Number(lost.textContent) === 5) {
+          dead.textContent = 0;
+          lost.textContent = 0;
+          alert('Вы проиграли');
+        }
+      }
+    };
+  }
 } else {
   // console.log('You are on the server');
 }
